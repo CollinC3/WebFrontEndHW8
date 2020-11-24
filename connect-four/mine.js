95dcfa255577d03c3010543900603e203039a37c
CollinC3 <collinc1997@gmail.com>
2020-11-23 18:19:39 -0800

    First Commit

---
+++ connect-four/mine.js
@@ -0,0 +1,193 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Square(props) {
+  return (
+    <div className={props.color} onClick={props.onClick}>
+      {props.value}
+    </div>
+  );
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        color={this.props.player[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+  
+  render() {
+    return (
+      <div>
+        <div className="board-row6">
+          {this.renderSquare(35)}
+          {this.renderSquare(36)}
+          {this.renderSquare(37)}
+          {this.renderSquare(38)}
+          {this.renderSquare(39)}
+          {this.renderSquare(40)}
+          {this.renderSquare(41)}
+        </div>
+        <div className="board-row5">
+          {this.renderSquare(28)}
+          {this.renderSquare(29)}
+          {this.renderSquare(30)}
+          {this.renderSquare(31)}
+          {this.renderSquare(32)}
+          {this.renderSquare(33)}
+          {this.renderSquare(34)}
+        </div>
+        <div className="board-row4">
+          {this.renderSquare(21)}
+          {this.renderSquare(22)}
+          {this.renderSquare(23)}
+          {this.renderSquare(24)}
+          {this.renderSquare(25)}
+          {this.renderSquare(26)}
+          {this.renderSquare(27)}
+        </div>
+        <div className="board-row3">
+          {this.renderSquare(14)}
+          {this.renderSquare(15)}
+          {this.renderSquare(16)}
+          {this.renderSquare(17)}
+          {this.renderSquare(18)}
+          {this.renderSquare(19)}
+          {this.renderSquare(20)}
+        </div>
+        <div className="board-row2">
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
+          {this.renderSquare(10)}
+          {this.renderSquare(11)}
+          {this.renderSquare(12)}
+          {this.renderSquare(13)}
+        </div>
+        <div className="board-row1">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(42).fill(null)
+        }
+      ],
+      redIsNext: true,
+      stepNumber: 0
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    console.log(squares);
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.redIsNext ? "Red" : "Blue";
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
+      stepNumber: history.length,
+      redIsNext: !this.state.redIsNext
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      redIsNext: (step % 2) === 0
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.redIsNext? "Red" : "Blue");
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            player={current.redIsNext}
+            onClick={i => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2, 3],
+    [4, 5, 6, 7],
+    [8, 9, 10, 11],
+    [12, 13, 14, 15],
+    [0, 4, 8, 12],
+    [1, 5, 9, 13],
+    [2, 6, 10, 14],
+    [3, 7, 11, 15],
+    [0, 5, 10, 15],
+    [12, 9, 6, 3]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c, d] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+  
---
+++ connect-four/src/game.js
@@ -0,0 +1,147 @@
+import React, { Component } from 'react';
+import './game.css';
+
+function Square(props){
+  return <div className="Square"><div className={props.value}></div></div>
+}
+
+function Column(props){
+    return <div className="Column" onClick={() => props.handleClick()}>
+      {[...Array(props.squares.length)].map((x, j) => 
+        <Square key={j} value={props.squares[j]}></Square>)}
+      </div>
+ }
+
+class Board extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      /*history: [
+        {
+          boardState: new Array(7).fill(new Array(6).fill(null))
+        }
+      ],*/
+      boardState: new Array(7).fill(new Array(6).fill(null)),
+      //stepNumber: 0,
+      playerTurn: 'Red',
+      winner: ''
+    }
+  }
+
+  selectedGame(){
+    this.setState({
+       boardState: new Array(7).fill(new Array(6).fill(null))
+    })
+  }
+
+  makeMove(columnID){
+    const boardCopy = this.state.boardState.map(function(arr) {
+      return arr.slice();
+    });
+    if( boardCopy[columnID].indexOf(null) !== -1 ){
+      let newColumn = boardCopy[columnID].reverse()
+      newColumn[newColumn.indexOf(null)] = this.state.playerTurn
+      newColumn.reverse()
+      this.setState({
+        playerTurn: (this.state.playerTurn === 'Red') ? 'Blue' : 'Red',
+        boardState: boardCopy
+      })
+    }
+  }
+
+  /*Only make moves if winner doesn't exist*/
+  handleClick(columnID) {
+    if(this.state.winner === ''){
+      this.makeMove(columnID)
+    }
+  }
+  
+  /*check the winner*/
+  componentDidUpdate(){
+    let winner = checkWinner(this.state.boardState)
+    console.log(this.state.boardState)
+    if(this.state.winner !== winner){
+      this.setState({winner: winner})
+    }
+  }
+
+  render(){
+
+    /*If a winner exists display the name*/
+    let winnerMessageStyle
+    if(this.state.winner !== ""){
+      winnerMessageStyle = "winnerMessage appear"
+    }else {
+      winnerMessageStyle = "winnerMessage"
+    }
+
+    /*Contruct columns allocating column from board*/
+    let columns = [...Array(this.state.boardState.length)].map((x, i) => 
+      <Column 
+          key={i}
+          squares={this.state.boardState[i]}
+          handleClick={() => this.handleClick(i)}
+      ></Column>
+    )
+
+    return (
+      <div>
+        {
+          <div className="Board">
+            {columns}
+          </div>
+        }
+        <div className={winnerMessageStyle}>{this.state.winner}</div>
+      </div>
+    )
+  }
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="Game">
+        <div className="Game-Board">
+          <Board></Board>
+        </div>
+      </div>
+    );
+  }
+}
+
+function checkLine(a,b,c,d) {
+  return ((a !== null) && (a === b) && (a === c) && (a === d));
+}
+
+function checkWinner(bs) {
+  for (let c = 0; c < 7; c++)
+    for (let r = 0; r < 4; r++)
+      if (checkLine(bs[c][r], bs[c][r+1], bs[c][r+2], bs[c][r+3]))
+        return bs[c][r] + ' wins!';
+
+  for (let r = 0; r < 6; r++)
+    for (let c = 0; c < 4; c++)
+      if (checkLine(bs[c][r], bs[c+1][r], bs[c+2][r], bs[c+3][r]))
+        return bs[c][r] + ' wins!';
+
+  for (let r = 0; r < 3; r++)
+    for (let c = 0; c < 4; c++)
+      if (checkLine(bs[c][r], bs[c+1][r+1], bs[c+2][r+2], bs[c+3][r+3]))
+        return bs[c][r] + ' wins!';
+
+  for (let r = 0; r < 4; r++)
+    for (let c = 3; c < 6; c++)
+      if (checkLine(bs[c][r], bs[c-1][r+1], bs[c-2][r+2], bs[c-3][r+3]))
+        return bs[c][r] + ' wins!';
+
+  for (let r = 0; r < 6; r++)
+    for (let c = 0; c < 7; c++)
+      if (bs[c][r] === null)
+        return "";
+
+  return "Draw";
+}
+
+export default App;
